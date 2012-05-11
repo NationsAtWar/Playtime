60c3f216eeec1c57d8870c86debb1ebcd193a485
Feldherren <rpaliwoda@googlemail.com>
2012-05-11 22:05:35 +0100

    Initial commit.

---
+++ src/org/nationsatwar/playtime/Playtime.java
@@ -0,0 +1,22 @@
+package org.nationsatwar.playtime;
+
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Playtime extends JavaPlugin
+{
+	Logger log;
+	
+	public void onEnable()
+	{
+		// read external file for event stuff
+		log = this.getLogger();
+		log.info("Your plugin has been enabled!");
+	}
+	
+	public void onDisable()
+	{
+		log.info("Your plugin has been disabled!");
+	}
+}
